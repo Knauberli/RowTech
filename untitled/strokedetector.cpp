6dbdf4fba3ea3fbf983fb96b2e6739e9416a1ab8
Qadriga <rob.sandhausen@freenet.de>
2018-09-25 17:18:31 +0200

    todo: improve some detection of the minimum

--- untitled/strokedetector.cpp
+++ untitled/strokedetector.cpp
@@ -9,6 +9,7 @@ StrokeDetector::StrokeDetector(QObject *parent) : QObject(parent)
     // generate filter an Gyro sensor and connect them together
     MA_Filter* fil = new MA_Filter(this);
     GyroSensor* sen = new GyroSensor(this);
+    sen->setPollingInterval(5);
     timer.start();
     connect(sen,SIGNAL(newSensorValue(GyroData)),fil,SLOT(updateInput(GyroData)));
     connect(fil,SIGNAL(updatedOutput(qreal)),this,SLOT(processnewvalue(qreal)));
@@ -23,10 +24,12 @@ StrokeDetector::~StrokeDetector(){
 
 void StrokeDetector::rundetection(){
     // if the last element is lower than the element before we passed a minimum
+    qDebug() << this->downSampleVlaues->size()-1 << this->downSampleVlaues->at(this->downSampleVlaues->size() - 2);
     if( this->downSampleVlaues->at(this->downSampleVlaues->size()-1) > this->downSampleVlaues->at(this->downSampleVlaues->size() - 2) ){
         // get time between to time stamps
         qint32 ellapsmsec = timer.elapsed();
         timer.restart();
+        qDebug() << "All good so far";
         ellapsmsec = msecs_in_minute / ellapsmsec; // dirived from ---> msecs_in_minute = spm * ellapsmsec
         quint8 res = ellapsmsec & 0xff;
         emit StrokeUpdate(res);
@@ -34,7 +37,7 @@ void StrokeDetector::rundetection(){
 }
 void StrokeDetector::processnewvalue(qreal newValue){
     static qint8 dscounter = 0; // init in static function --> creepy shit
-    if( (dscounter++ % ds_ration) == 0){
+    if((dscounter++ % ds_ration) == 0){
         this->downSampleVlaues->put_data(newValue);
         rundetection();
     }
